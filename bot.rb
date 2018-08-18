cea5372b68cdee7e992f136ddde7d914b69065b0
andreydro <andreydro14@gmail.com>
2018-08-19 02:32:42 +0300

    Add basic scale

--- bot.rb
+++ bot.rb
@@ -1,59 +1,60 @@
 require 'telegram/bot'
 require 'pry'
+require "ohm"
 
 TOKEN = '684307416:AAGNnNrsbMxQdX9JyCjKLC2qHo0f71XH7jo'
 
+@language
+
 Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
-  	msg = message.text.downcase
-    case msg
-    when 'start'
-      bot.api.sendMessage(
-      	chat_id: message.chat.id, 
-      	text: %(
-      	  Hello, #{message.from.first_name}. 
-      	  Do you want see basic vacabulary or translate particular phrase?
-      	  use commands basic or phrase
-      	  )
-      	)
+    case message
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+
+    when Telegram::Bot::Types::CallbackQuery
+      # 2 main featrures
+      if message.data == 'basic'
+        kb = [
+          Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Conversation', callback_data: 'conversation'),
+          Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Travel', callback_data: 'travel'),
+          Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Hotel', callback_data: 'hotel')
+          ]
+        markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
+
+        bot.api.send_message(chat_id: message.from.id, text: "Choose category", reply_markup: markup)
+      end
+
+      if message.data == 'translation'
+        bot.api.send_message(chat_id: message.from.id, text: "Cooming soon...")
+      end
+
+      # language
+      if message.data == 'french'
+         kb = [
+          Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Basic phrases', callback_data: 'basic'),
+          Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Translation', callback_data: 'translation')
+          ]
+        markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
+
+        bot.api.send_message(chat_id: message.from.id, text: "Do you want see list of basic phrases or translation function", reply_markup: markup)
+      end
+
+      # category
+      
+    when Telegram::Bot::Types::Message
+      kb = [
+        Telegram::Bot::Types::InlineKeyboardButton.new(text: 'French', callback_data: 'french'),
+        Telegram::Bot::Types::InlineKeyboardButton.new(text: 'German', callback_data: 'german'),
+        Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Spanish', callback_data: 'spanish')
+      ]
+      markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
+      bot.api.send_message(chat_id: message.chat.id, text: 'Please, choose language you want to use', reply_markup: markup) 
+
     when 'help'
       bot.api.sendMessage(
       	chat_id: message.chat.id, 
         text: "You can use following commands"
-      )
-    when 'basic'
-		  bot.api.sendMessage(
-		    	chat_id: message.chat.id, 
-		      text: %(
-		        Please, choose language:
-		          French
-		          German
-		          Spanish
-		      )
-		    )
-    when 'phrase'
-	    bot.api.sendMessage(
-	      	chat_id: message.chat.id, 
-	        text: "You will able to use realtime translation soon...."
-	      )  	 
-    when 'french'
-    	bot.api.sendMessage(
-      	chat_id: message.chat.id, 
-        text: %(
-          Enter category:
-          Conversation
-          Useful
-          Guest
-          Travel
-          Hotel
-          Restaurant
-          Services
-          Stores
-          Health
-          Culture
-          Activity
-          Job
-        )
       )
     end
   end
